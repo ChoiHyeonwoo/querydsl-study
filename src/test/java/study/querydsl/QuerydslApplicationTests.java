10b798e5008afca5a2f17347bd2208fcf4cd8fba
최현우 <escape0619@gmail.com>
2020-12-31 14:52:20 +0900

    db 설정 및 라이브러리 추가 implementation 'com.github.gavlyukovskiy:p6spy-spring-boot-starter:1.5.8'

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -4,6 +4,7 @@ import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Commit	// 실제 데이터에 남기기위해! -> 공부할때만 실무는 지우기
 class QuerydslApplicationTests {
 
 	/*@PersistenceContext java 표준 스펙. */
