e18e2120c17892afcb628c67f904d45109a1468f
최현우 <escape0619@gmail.com>
2021-01-19 09:14:17 +0900

    Querydsl Web 단점: 1.controller에서 Querydsl에 의존하는 형태가 발생될 수 있음. 정말 정말 사용시에는 득보다 실이 너무많다 실무에서 쓰기 한계가 너무 명확.

--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -7,7 +7,7 @@ import study.querydsl.entity.Member;
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor<Member> {
-    // QuerydslPredicateExecutor
+    // QuerydslPredicateExecutor, Querydsl Web!!
     // 단점: 조인을 못함, parameter가 predicate라서 service or controller에서 Querydsl에 의존하는 형태가 발생될 수 있음.
     // 실무에서 쓰기 한계가 있음.
     List<Member> findByUsername(String username);
